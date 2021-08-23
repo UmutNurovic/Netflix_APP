f4a7b5b0c4fe5ffbea90e0afa6fcc142fbb9604a
umutcan <akturkumutcan58@gmail.com>
2021-08-23 20:54:10 +0300

    finished
    finished

--- api/index.js
+++ api/index.js
@@ -1,23 +1,32 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const authRoute = require('./routes/auth');
-const userRoute  = require('./routes/user');
-const movieRoute  = require('./routes/movie');
-const listsRoute  = require('./routes/lists');
-mongoose.connect(process.env.MONGO_DB_CONNECT,
-{ useUnifiedTopology: true ,useNewUrlParser: true})
-.then(()=>console.log('connection DB'))
-.catch((err)=>console.log(err));
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const movieRoute = require("./routes/movies");
+const listRoute = require("./routes/lists");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
 app.use(express.json());
 
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/movies", movieRoute);
+app.use("/api/lists", listRoute);
 
-app.use('/api/auth',authRoute);
-app.use('/api/users',userRoute);
-app.use('/api/movie',movieRoute);
-app.use('/api/lists',listsRoute);
-
-const port = process.env.PORT || 3000;
-
-app.listen(port, () => console.log(`Server running on port ${port} 🔥`));
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,10 +1,13 @@
 import { ArrowDropDown, Notifications, Search } from "@material-ui/icons";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import "./navbar.scss";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../authContext/AuthContext";
+import { logout } from "../../authContext/AuthActions";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const { dispatch } = useContext(AuthContext);
 
   window.onscroll = () => {
     setIsScrolled(window.pageYOffset === 0 ? false : true);
@@ -18,17 +21,15 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
-           <Link to ="/" className="link">
-          <span>Homepage</span>
+          <Link to="/" className="link">
+            <span>Homepage</span>
           </Link>
-          <Link to ="/series" className="link">
-          <span>Series</span>
+          <Link to="/series" className="link">
+            <span className="navbarmainLinks">Series</span>
           </Link>
-          <Link to ="/movies" className="link">
-          <span>Movies</span>
+          <Link to="/movies" className="link">
+            <span className="navbarmainLinks">Movies</span>
           </Link>
-          
-          
           <span>New and Popular</span>
           <span>My List</span>
         </div>
@@ -44,7 +45,7 @@ const Navbar = () => {
             <ArrowDropDown className="icon" />
             <div className="options">
               <span>Settings</span>
-              <span>Logout</span>
+              <span onClick={() => dispatch(logout())}>Logout</span>
             </div>
           </div>
         </div>
